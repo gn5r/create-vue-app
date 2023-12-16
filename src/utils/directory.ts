570fb686b0e6de84d32166bbed7d661b7ca7a222
gn5r <32511242+gn5r@users.noreply.github.com>
2023-12-17 00:20:29 +0900

    feat: add lint-staged and rendering ejs template (#10)
    * build: add ejs
    
    * chore: add null to usePackageManager's Union type
    
    * feat: add preDirectoryTraverse function and postDirectoryTraverse function
    
    * feat: add lint-staged and rendering ejs template

--- src/utils/directory.ts
+++ src/utils/directory.ts
@@ -1,19 +1,55 @@
-import { existsSync, readdirSync, lstatSync, unlinkSync } from "node:fs";
+import fs from "node:fs";
 import { resolve } from "node:path";
 
 function emptyDir(dir: string) {
-  if (!existsSync(dir)) return;
+  if (!fs.existsSync(dir)) return;
 
-  for (const filename of readdirSync(dir)) {
+  for (const filename of fs.readdirSync(dir)) {
     if (filename === ".git") continue;
     const path = resolve(dir, filename);
 
-    if (lstatSync(path).isDirectory()) {
+    if (fs.lstatSync(path).isDirectory()) {
       emptyDir(path);
     } else {
-      unlinkSync(path);
+      fs.unlinkSync(path);
     }
   }
 }
 
-export { emptyDir };
+function preDirectoryTraverse(
+  dir: string,
+  dirCallback: Function,
+  fileCallback: Function
+) {
+  for (const filename of fs.readdirSync(dir)) {
+    if (filename === ".git") continue;
+    const path = resolve(dir, filename);
+    if (fs.lstatSync(path).isDirectory()) {
+      dirCallback(path);
+      if (fs.existsSync(path)) {
+        preDirectoryTraverse(path, dirCallback, fileCallback);
+      }
+      continue;
+    }
+    fileCallback(path);
+  }
+}
+
+function postDirectoryTraverse(
+  dir: string,
+  dirCallback: Function,
+  fileCallback: Function
+) {
+  for (const filename of fs.readdirSync(dir)) {
+    if (filename === ".git") continue;
+    const path = resolve(dir, filename);
+    if (fs.lstatSync(path).isDirectory()) {
+      postDirectoryTraverse(path, dirCallback, fileCallback);
+      dirCallback(path);
+      continue;
+    }
+    fileCallback(path);
+  }
+}
+
+export { emptyDir, preDirectoryTraverse, postDirectoryTraverse };
