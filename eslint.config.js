bbb785ab72493ab588796187a08eb16c1feaff48
gn5r <shangyuan.tuolang@gmail.com>
2024-05-02 01:31:48 +0900

    chore: refactoring eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,19 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
+  { ignores: ["node_modules", "dist", "template"] },
   { languageOptions: { globals: globals.node } },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
       "@typescript-eslint/ban-types": "warn",
+    },
+  },
+  {
+    files: ["**/*.cjs"],
+    rules: {
+      "@typescript-eslint/no-var-requires": "off",
     },
   },
 ];
