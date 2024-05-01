df8578834878c39640bdb69da2cb9952bb8e4ce5
gn5r <shangyuan.tuolang@gmail.com>
2024-05-02 00:26:26 +0900

    fix: fix `import.meta.BASE_URL` to `import.meta.env.BASE_URL`

--- template/vueLayouts/src/plugins/router.ts
+++ template/vueLayouts/src/plugins/router.ts
@@ -1,10 +1,10 @@
-import { setupLayouts } from "virtual:generated-layouts";
 import { createRouter, createWebHistory } from "vue-router";
+import { setupLayouts } from "virtual:generated-layouts";
 
 export const routes = setupLayouts([]);
 
 const router = createRouter({
-  history: createWebHistory(import.meta.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
